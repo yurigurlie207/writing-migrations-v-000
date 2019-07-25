27e06b4cf3761888c46a2ff174a3ef0cecf71211
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-25 02:01:33 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
   def change
-    add_column :artists, :favorite_food, :string
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 end
